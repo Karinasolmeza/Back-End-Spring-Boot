7969314a0665fd2e2ce7274f76604d18dd219a37
KarinaSol <karinasolmeza83@gmail.com>
2023-01-07 18:00:26 -0300

    net

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 
-FROM amazoncorretto:8-alpine-jdk
+# Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+# Click nbfs://nbhost/SystemFileSystem/Templates/Other/Dockerfile to edit this template
+
+FROM openjdk:8
 
 
 
--- src/main/java/com/backEndSpringBoot/Spring/Boot/Controller/AuthController.java
+++ src/main/java/com/backEndSpringBoot/Spring/Boot/Controller/AuthController.java
@@ -6,6 +6,7 @@ import com.backEndSpringBoot.Spring.Boot.Model.dto.UserDto;
 import com.backEndSpringBoot.Spring.Boot.Service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
